8147d1c5240f9a4ad3a3d61e6952ae889c4357dd
James Xie <jamesxie2019@u.northwestern.edu>
2019-04-29 19:54:11 -0500

    product titles displayed in an unstyled list

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Product from './Components/Product.js'
+
+class App extends React.Component {
+
+  // ProductList = ()=> {
+  //   console.log(this.props)
+  //   const products = this.props.products
+  //   const listItems = Object.keys(products).map( (key) =>
+  //     <Product
+  //       title={products[key].title}
+  //     />
+  //   );
+  //   return (
+  //     listItems
+  //   );
+  // }
+
+  productList = () => {
+    const products = this.props.products
+    const productkeys = Object.keys(products);
+    const items = productkeys.map( (key) => 
+      <Product 
+        title={products[key].title} 
+        description={products[key].description}
+        price={products[key].price} 
+        sizes={products[key].availableSizes}
+        image={products[key].sku}
+      />
+      );
+    return items;
+  }
+
+  render() {
+    return (
+      <div className="App-container">
+        {this.productList()}
+      </div>
+    );
+  }
 }
 
 export default App;
